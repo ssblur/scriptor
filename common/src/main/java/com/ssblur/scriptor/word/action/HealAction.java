d85e5375927d591f952414d3e238a2a725d9c981
Patrick Hallbick <patrick.hallbick@outlook.com>
2023-04-01 01:05:35 -0400

    Add item targeting to heal enchant

--- common/src/main/java/com/ssblur/scriptor/word/action/HealAction.java
+++ common/src/main/java/com/ssblur/scriptor/word/action/HealAction.java
@@ -1,6 +1,7 @@
 package com.ssblur.scriptor.word.action;
 
 import com.ssblur.scriptor.helpers.targetable.EntityTargetable;
+import com.ssblur.scriptor.helpers.targetable.ItemTargetable;
 import com.ssblur.scriptor.helpers.targetable.Targetable;
 import com.ssblur.scriptor.word.descriptor.Descriptor;
 import com.ssblur.scriptor.word.descriptor.DurationDescriptor;
@@ -15,6 +16,16 @@ public class HealAction extends Action {
     for(var d: descriptors) {
       if(d instanceof StrengthDescriptor strengthDescriptor)
         strength += strengthDescriptor.strengthModifier();
+    }
+
+    if(targetable instanceof ItemTargetable itemTargetable) {
+      var item = itemTargetable.getTargetItem();
+      if(item != null && !item.isEmpty()) {
+        if(item.isDamageableItem()) {
+          item.setDamageValue(item.getDamageValue() - (int) Math.round(strength));
+          return;
+        }
+      }
     }
 
     if(targetable instanceof EntityTargetable entityTargetable) {
