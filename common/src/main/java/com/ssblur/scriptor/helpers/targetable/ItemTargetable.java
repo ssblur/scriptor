241da8f90978cd9f523dbb5a8e66fa297ff3426e
Patrick Hallbick <patrick.hallbick@outlook.com>
2023-04-07 18:53:35 -0400

    Allow Item targeting without using the item by default

--- common/src/main/java/com/ssblur/scriptor/helpers/targetable/ItemTargetable.java
+++ common/src/main/java/com/ssblur/scriptor/helpers/targetable/ItemTargetable.java
@@ -6,13 +6,22 @@ import net.minecraft.world.item.ItemStack;
 
 public class ItemTargetable extends EntityTargetable {
   ItemStack itemStack;
+  boolean targetItemByDefault;
 
   public ItemTargetable(ItemStack itemStack, Player entity) {
     super(entity);
     this.itemStack = itemStack;
+    this.targetItemByDefault = true;
   }
 
   public ItemStack getTargetItem() {
     return itemStack;
+  }
+
+  public boolean shouldTargetItemByDefault() { return targetItemByDefault; }
+
+  public ItemTargetable withTargetItemByDefault(boolean targetItemByDefault) {
+    this.targetItemByDefault = targetItemByDefault;
+    return this;
   }
 }
