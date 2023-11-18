2e03a681238f8c96f535da930640b01f8f054f8c
Patrick Hallbick <patrick.hallbick@outlook.com>
2023-11-18 05:38:15 -0500

    Add new Magic particles

---
+++ common/src/main/java/com/ssblur/scriptor/particle/MagicParticle.java
@@ -0,0 +1,81 @@
+package com.ssblur.scriptor.particle;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.BufferBuilder;
+import com.mojang.blaze3d.vertex.DefaultVertexFormat;
+import com.mojang.blaze3d.vertex.Tesselator;
+import com.mojang.blaze3d.vertex.VertexFormat;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.particle.ParticleRenderType;
+import net.minecraft.client.particle.TextureSheetParticle;
+import net.minecraft.client.renderer.texture.AbstractTexture;
+import net.minecraft.client.renderer.texture.TextureAtlas;
+import net.minecraft.client.renderer.texture.TextureManager;
+import org.lwjgl.opengl.GL11;
+
+public class MagicParticle extends TextureSheetParticle {
+
+  public MagicParticle(
+    ClientLevel level,
+    double d,
+    double e,
+    double f,
+    double xd,
+    double yd,
+    double zd,
+    float size,
+    float red,
+    float green,
+    float blue,
+    float age
+  ) {
+    super(level, d, e, f, 0, 0, 0);
+
+    this.xd = xd + (Math.random() / 20 - 0.025f);
+    this.yd = yd + (Math.random() / 20 - 0.025f);
+    this.zd = zd + (Math.random() / 20 - 0.025f);
+
+    this.rCol = red / 255f;
+    this.gCol = green / 255f;
+    this.bCol = blue / 255f;
+    this.alpha = 1.0f;
+    this.gravity = 0;
+    this.quadSize = size;
+    this.lifetime = (int) ((Math.random() * 0.4d + 0.6d) * 30 * age);
+    this.xo = x;
+    this.yo = y;
+    this.zo = z;
+    this.hasPhysics = true;
+
+    setSize(0.01f, 0.01f);
+  }
+
+  @Override
+  public void tick() {
+    super.tick();
+
+    this.alpha *= 0.9f;
+  }
+
+  @Override
+  public float getQuadSize(float p_217561_1_) {
+    float agescale = (float) age / 30;
+    if (agescale > 1F) {
+      agescale = 2 - agescale;
+    }
+
+    quadSize = agescale * 0.5F;
+    return quadSize;
+  }
+
+  @Override
+  protected int getLightColor(float partialTicks) {
+    return 0xF000F0;
+  }
+
+  @Override
+  public ParticleRenderType getRenderType() {
+    return ParticleRenderType.PARTICLE_SHEET_TRANSLUCENT;
+  }
+}
---
+++ common/src/main/java/com/ssblur/scriptor/particle/package-info.java
@@ -0,0 +1,4 @@
+@ParametersAreNonnullByDefault
+package com.ssblur.scriptor.particle;
+
+import javax.annotation.ParametersAreNonnullByDefault;
