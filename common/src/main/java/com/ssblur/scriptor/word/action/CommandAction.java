be6e2988af00085e1839be13aaebbcb5e3821c4a
Patrick Hallbick <patrick.hallbick@outlook.com>
2024-01-29 06:39:41 -0500

    Add item targeting to custom spells

--- common/src/main/java/com/ssblur/scriptor/word/action/CommandAction.java
+++ common/src/main/java/com/ssblur/scriptor/word/action/CommandAction.java
@@ -1,25 +1,22 @@
 package com.ssblur.scriptor.word.action;
 
-import com.mojang.brigadier.CommandDispatcher;
+import com.ssblur.scriptor.helpers.ItemTargetableHelper;
 import com.ssblur.scriptor.helpers.targetable.EntityTargetable;
+import com.ssblur.scriptor.helpers.targetable.InventoryTargetable;
 import com.ssblur.scriptor.helpers.targetable.ItemTargetable;
 import com.ssblur.scriptor.helpers.targetable.Targetable;
 import com.ssblur.scriptor.word.descriptor.Descriptor;
 import net.minecraft.commands.CommandSource;
 import net.minecraft.commands.CommandSourceStack;
-import net.minecraft.data.models.blockstates.VariantProperties;
-import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.entity.decoration.ArmorStand;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.phys.Vec2;
-import net.minecraft.world.phys.Vec3;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -66,11 +63,6 @@ public class CommandAction extends Action {
     return Cost.add(cost);
   }
 
-  public boolean applyToItem(Targetable caster, ItemTargetable targetable, Descriptor[] descriptors) {
-    // TODO
-    return false;
-  }
-
   public static String getTargetSelector(Entity entity) {
     var uuid = entity.getUUID();
     long l = uuid.getLeastSignificantBits();
@@ -80,22 +72,20 @@ public class CommandAction extends Action {
 
   @Override
   public void apply(Targetable caster, Targetable targetable, Descriptor[] descriptors) {
-    if(targetable instanceof ItemTargetable itemTargetable && itemTargetCommand != null)
-      if(applyToItem(caster, itemTargetable, descriptors))
-        return;
-
     // @caster is a fake selector that's replaced at runtime.
     // It will target the entity or block position that cast this spell.
+    // @target is a fake selector that is also replaced at runtime.
+    // It will target the target of this spell, which is useful in item targeted commands.
     // This is run using execute.
     // By default, this is run as the target, with @s targeting them.
     String casterString = "@s";
     ArmorStand tempCasterEntity = null;
+    ArmorStand tempItemEntity = null;
 
     if(caster instanceof EntityTargetable entityTargetable) {
       var entity = entityTargetable.getTargetEntity();
       casterString = getTargetSelector(entity);
     } else {
-      // summon an entity for targeting.
       var level = (ServerLevel) caster.getLevel();
       tempCasterEntity = EntityType.ARMOR_STAND.spawn(level, caster.getOrigin(), MobSpawnType.COMMAND);
       if(tempCasterEntity != null) {
@@ -106,16 +96,40 @@ public class CommandAction extends Action {
 
     List<String> commandToParse = new ArrayList<>();
     var pos = targetable.getTargetPos();
-    if(targetable instanceof EntityTargetable entityTargetable && entityTargetCommand != null) {
+    if(!ItemTargetableHelper.getTargetItemStack(targetable).isEmpty() && !itemTargetCommand.isEmpty()) {
+      var targetString = "@s";
+      if(targetable instanceof  EntityTargetable entityTargetable)
+        targetString = getTargetSelector(entityTargetable.getTargetEntity());
+      if(caster.getLevel() instanceof ServerLevel level) {
+        tempItemEntity = EntityType.ARMOR_STAND.spawn(level, targetable.getTargetBlockPos(), MobSpawnType.COMMAND);
+        if (tempItemEntity != null) {
+          tempItemEntity.setItemSlot(EquipmentSlot.MAINHAND, ItemTargetableHelper.getTargetItemStack(targetable).copy());
+          tempItemEntity.setCustomName(Component.translatable("command.scriptor.magic_name"));
+          for (var i : itemTargetCommand)
+            commandToParse.add(
+              String.format(
+                "execute at %s as %s run %s",
+                getTargetSelector(tempItemEntity),
+                getTargetSelector(tempItemEntity),
+                i.replace("@caster", casterString).replace("@target", targetString)
+              )
+            );
+        }
+      } else {
+        if(caster instanceof EntityTargetable entityTargetable && entityTargetable.getTargetEntity() instanceof Player player)
+          player.sendSystemMessage(Component.translatable("command.scriptor.no_item_commands_creative"));
+      }
+    } else if(targetable instanceof EntityTargetable entityTargetable && !entityTargetCommand.isEmpty()) {
       var entity = entityTargetable.getTargetEntity();
+      var targetString = getTargetSelector(entity);
       var uuid = entity.getUUID();
       for(var i: entityTargetCommand)
         commandToParse.add(
           String.format(
             "execute at %s as %s run %s",
-            getTargetSelector(entity),
-            getTargetSelector(entity),
-            i.replace("@caster", casterString)
+            targetString,
+            targetString,
+            i.replace("@caster", casterString).replace("@target", targetString)
           )
         );
     } else {
@@ -127,7 +141,7 @@ public class CommandAction extends Action {
             pos.y,
             pos.z,
             targetable.getLevel().dimension().location(),
-            i.replace("@caster", casterString)
+            i.replace("@caster", casterString).replace("@target", "@e[distance=..1]")
           )
         );
     }
@@ -154,5 +168,15 @@ public class CommandAction extends Action {
 
     if(tempCasterEntity != null)
       tempCasterEntity.remove(Entity.RemovalReason.DISCARDED);
+    if(tempItemEntity != null) {
+      if(targetable instanceof InventoryTargetable inventoryTargetable) {
+        if (inventoryTargetable.getContainer() != null)
+          inventoryTargetable.getContainer().setItem(inventoryTargetable.getTargetedSlot(), tempItemEntity.getItemBySlot(EquipmentSlot.MAINHAND));
+      } else if(targetable instanceof ItemTargetable itemTargetable) {
+        itemTargetable.getTargetItem().setCount(0);
+        ItemTargetableHelper.depositItemStack(itemTargetable, tempItemEntity.getItemBySlot(EquipmentSlot.MAINHAND));
+      }
+      tempItemEntity.remove(Entity.RemovalReason.DISCARDED);
+    }
   }
 }
