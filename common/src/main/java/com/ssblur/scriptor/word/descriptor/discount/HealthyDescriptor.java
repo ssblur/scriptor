51c1da6350cff40d3e099c27ac04215e35462517
Patrick Hallbick <patrick.hallbick@outlook.com>
2024-09-20 09:50:18 -0400

    Slightly rebalance Healthy descriptor

--- common/src/main/java/com/ssblur/scriptor/word/descriptor/discount/HealthyDescriptor.java
+++ common/src/main/java/com/ssblur/scriptor/word/descriptor/discount/HealthyDescriptor.java
@@ -9,13 +9,13 @@ import net.minecraft.world.entity.LivingEntity;
 public class HealthyDescriptor extends Descriptor implements CastDescriptor {
   @Override
   public Cost cost() {
-    return new Cost(0.8d, COSTTYPE.MULTIPLICATIVE);
+    return new Cost(0.9d, COSTTYPE.MULTIPLICATIVE);
   }
 
   @Override
   public boolean cannotCast(Targetable caster) {
     if(caster instanceof EntityTargetable entityTargetable && entityTargetable.getTargetEntity() instanceof LivingEntity living)
-      return living.getHealth() < living.getMaxHealth();
+      return living.getHealth() < (living.getMaxHealth() * 0.9);
     return true;
   }
 }
