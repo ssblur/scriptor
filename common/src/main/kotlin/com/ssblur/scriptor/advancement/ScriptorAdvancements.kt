772fd98e09e23b0fe6168f89073cba739af10ba9
Patrick Hallbick <patrick.hallbick@outlook.com>
2025-01-19 06:09:08 -0500

    Migrate to unfocused advancement triggers

--- common/src/main/kotlin/com/ssblur/scriptor/advancement/ScriptorAdvancements.kt
+++ common/src/main/kotlin/com/ssblur/scriptor/advancement/ScriptorAdvancements.kt
@@ -1,17 +1,18 @@
 package com.ssblur.scriptor.advancement
 
-import com.ssblur.scriptor.ScriptorMod
+import com.ssblur.scriptor.ScriptorMod.location
 import com.ssblur.scriptor.ScriptorMod.registerTrigger
+import com.ssblur.unfocused.advancement.GenericTrigger
 
 object ScriptorAdvancements {
-    val TOME = registerTrigger("tome_collect") { GenericScriptorTrigger(ScriptorMod.location("tome_collect")) }
-    val TOME_1 = registerTrigger("tome1_collect") { GenericScriptorTrigger(ScriptorMod.location("tome1_collect")) }
-    val TOME_2 = registerTrigger("tome2_collect") { GenericScriptorTrigger(ScriptorMod.location("tome2_collect")) }
-    val TOME_3 = registerTrigger("tome3_collect") { GenericScriptorTrigger(ScriptorMod.location("tome3_collect")) }
-    val TOME_4 = registerTrigger("tome4_collect") { GenericScriptorTrigger(ScriptorMod.location("tome4_collect")) }
-    val COMPLEX_SPELL = registerTrigger("complex_spell") { GenericScriptorTrigger(ScriptorMod.location("complex_spell")) }
-    val FIZZLE = registerTrigger("fizzle") { GenericScriptorTrigger(ScriptorMod.location("fizzle")) }
-    val COMMUNITY= registerTrigger("community") { GenericScriptorTrigger(ScriptorMod.location("community")) }
+    val TOME = registerTrigger("tome_collect") { GenericTrigger(location("tome_collect")) }
+    val TOME_1 = registerTrigger("tome1_collect") { GenericTrigger(location("tome1_collect")) }
+    val TOME_2 = registerTrigger("tome2_collect") { GenericTrigger(location("tome2_collect")) }
+    val TOME_3 = registerTrigger("tome3_collect") { GenericTrigger(location("tome3_collect")) }
+    val TOME_4 = registerTrigger("tome4_collect") { GenericTrigger(location("tome4_collect")) }
+    val COMPLEX_SPELL = registerTrigger("complex_spell") { GenericTrigger(location("complex_spell")) }
+    val FIZZLE = registerTrigger("fizzle") { GenericTrigger(location("fizzle")) }
+    val COMMUNITY = registerTrigger("community") { GenericTrigger(location("community")) }
 
     fun register() {}
 }
