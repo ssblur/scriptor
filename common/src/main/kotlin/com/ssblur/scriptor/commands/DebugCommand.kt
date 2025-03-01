fb3aa6f2615b73056cabde348d0f48450e197cf8
Patrick Hallbick <patrick.hallbick@outlook.com>
2025-02-28 19:09:30 -0500

    Update debug command

--- common/src/main/kotlin/com/ssblur/scriptor/commands/DebugCommand.kt
+++ common/src/main/kotlin/com/ssblur/scriptor/commands/DebugCommand.kt
@@ -1,9 +1,11 @@
 package com.ssblur.scriptor.commands
 
+import com.google.gson.Gson
 import com.mojang.brigadier.Command
 import com.mojang.brigadier.CommandDispatcher
 import com.mojang.brigadier.context.CommandContext
 import com.ssblur.scriptor.ScriptorMod.LOGGER
+import com.ssblur.scriptor.item.ScriptorTags
 import com.ssblur.scriptor.registry.words.WordRegistry.actionRegistry
 import com.ssblur.scriptor.registry.words.WordRegistry.descriptorRegistry
 import com.ssblur.scriptor.registry.words.WordRegistry.subjectRegistry
@@ -11,6 +13,7 @@ import net.minecraft.client.resources.language.I18n
 import net.minecraft.commands.CommandBuildContext
 import net.minecraft.commands.CommandSourceStack
 import net.minecraft.commands.Commands
+import net.minecraft.core.registries.Registries
 import net.minecraft.network.chat.Component
 import net.minecraft.world.entity.player.Player
 
@@ -25,6 +28,13 @@ object DebugCommand {
       Commands.literal("scriptor_debug")
         .requires { s: CommandSourceStack -> s.hasPermission(4) }
         .executes { execute(it) }
+    )
+    dispatcher.register(
+      Commands.literal("scriptor_debug").then(
+        Commands.literal("dump")
+          .requires { s: CommandSourceStack -> s.hasPermission(4) }
+          .executes { executeDump(it) }
+      )
     )
   }
 
@@ -45,6 +55,27 @@ object DebugCommand {
         message.append("Description not localized for subject.scriptor.").append(subject).append("\n")
       }
     }
+    if (command.source.entity is Player) {
+      (command.source.entity as Player).sendSystemMessage(Component.literal(message.toString()))
+    } else {
+      LOGGER.info(message.toString())
+    }
+    return Command.SINGLE_SUCCESS
+  }
+
+  private fun executeDump(command: CommandContext<CommandSourceStack>): Int {
+    val message = StringBuilder("Item list dumped as JSON")
+    var keys = command.source.level.registryAccess().registry(Registries.ITEM).get().entrySet().map {
+      it.key.location().toString()
+    }
+    val gson = Gson()
+    LOGGER.info(gson.toJson(keys))
+
+    keys = command.source.level.registryAccess().registry(Registries.ITEM).get()
+      .getTag(ScriptorTags.SPELLBOOKS).get().map {
+        it.unwrapKey().get().location().toString()
+    }
+    LOGGER.info(gson.toJson(keys))
     if (command.source.entity is Player) {
       (command.source.entity as Player).sendSystemMessage(Component.literal(message.toString()))
     } else {
