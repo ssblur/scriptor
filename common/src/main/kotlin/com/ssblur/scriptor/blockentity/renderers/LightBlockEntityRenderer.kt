a9fe73c305d55adcafb78e11228cfa32ca73e1cb
Patrick Hallbick <patrick.hallbick@outlook.com>
2025-03-03 22:35:16 -0500

    Switch Light block to magic particles

--- common/src/main/kotlin/com/ssblur/scriptor/blockentity/renderers/LightBlockEntityRenderer.kt
+++ common/src/main/kotlin/com/ssblur/scriptor/blockentity/renderers/LightBlockEntityRenderer.kt
@@ -3,13 +3,12 @@ package com.ssblur.scriptor.blockentity.renderers
 import com.mojang.blaze3d.vertex.PoseStack
 import com.ssblur.scriptor.blockentity.LightBlockEntity
 import com.ssblur.scriptor.color.CustomColors.getColor
+import com.ssblur.scriptor.particle.MagicParticleData
 import net.minecraft.client.Minecraft
 import net.minecraft.client.renderer.MultiBufferSource
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider
-import net.minecraft.core.particles.DustParticleOptions
 import org.joml.Random
-import org.joml.Vector3f
 
 class LightBlockEntityRenderer(@Suppress("unused_parameter") context: BlockEntityRendererProvider.Context?):
   BlockEntityRenderer<LightBlockEntity> {
@@ -35,7 +34,7 @@ class LightBlockEntityRenderer(@Suppress("unused_parameter") context: BlockEntit
     val zd = 0.25f + random.nextFloat() / 2f
 
     if (!Minecraft.getInstance().isPaused) light.level!!.addParticle(
-      DustParticleOptions(Vector3f(r, g, b), 1.0f),
+      MagicParticleData.magic(r.toInt(), g.toInt(), b.toInt()),
       (light.blockPos.x + xd).toDouble(),
       (light.blockPos.y + yd).toDouble(),
       (light.blockPos.z + zd).toDouble(),
