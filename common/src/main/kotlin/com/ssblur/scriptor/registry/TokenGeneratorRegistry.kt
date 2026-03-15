4cb99988e128cdc059dbae06b6f391e2f8cd1eee
Juniper Hallbick <pat.hallbick@outlook.com>
2026-03-15 18:57:50 -0400

    Rename object for convenience

--- common/src/main/kotlin/com/ssblur/scriptor/registry/TokenGeneratorRegistry.kt
+++ common/src/main/kotlin/com/ssblur/scriptor/registry/TokenGeneratorRegistry.kt
@@ -43,8 +43,8 @@ object TokenGeneratorRegistry {
     defaultGenerator = generator
   }
 
-  fun generateWord(word: String, `object`: JsonObject? = generatorBindingConfig[word]): String {
-    return getGenerator(getBinding(word))!!.generateToken(word, `object`)
+  fun generateWord(word: String, json: JsonObject? = generatorBindingConfig[word]): String {
+    return getGenerator(getBinding(word))!!.generateToken(word, json)
   }
 
   val MIXED_GROUP: TokenGeneratorGenerator = registerGeneratorGenerator("mixed_groups") { MixedGroupGenerator(it) }
